07ca17d65029f436cc25f2905dac28c3521ba897
Bill Sheng <billxsheng@gmail.com>
2018-02-19 20:08:31 -0500

    Added new pages/Making the website responsive.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-/* Navigation scroll */
+// Navigation scroll 
     $(function() {
      var offset = -47;    
       $('a[href*="#"]:not([href="#"])').click(function() {
@@ -16,6 +16,29 @@ $(document).ready(function() {
         }
       });
     });
+});
+
+if (window.location.hash)
+    scroll(0,0);
+// takes care of some browsers issue
+setTimeout(function(){scroll(0,0);},1);
+
+$(function(){
+//your current click function
+$('.scroll').on('click',function(e){
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop:$($(this).attr('href')).offset().top + 'px'
+    },1000,'swing');
+});
+
+// if we have anchor on the url (calling from other page)
+if(window.location.hash){
+    // smooth scroll to the anchor id
+    $('html,body').animate({
+        scrollTop:$(window.location.hash).offset().top + 'px'
+        },1000,'swing');
+    }
 });
 
 
