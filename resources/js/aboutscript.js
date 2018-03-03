3924f8386616e609d661d82fc0b1cc1405b92a55
Bill Sheng <billxsheng@gmail.com>
2018-03-03 01:47:14 -0500

    Optimized images.

--- resources/js/aboutscript.js
+++ resources/js/aboutscript.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
     $('.js--right').waypoint(function(direction) {
         $('.js--right').addClass('animated slideInRight');
     }, {
-        offset: '50%'
+        offset: '80%'
 });
     $('.js--left').waypoint(function(direction) {
         $('.js--left').addClass('animated slideInLeft');
     }, {
-        offset: '50%'
+        offset: '80%'
 });
     $('.js--left2').waypoint(function(direction) {
         $('.js--left2').addClass('animated slideInLeft');
     }, {
-        offset: '50%'
+        offset: '80%'
 });
     $('.js--left3').waypoint(function(direction) {
         $('.js--left3').addClass('animated slideInLeft');
--- resources/js/projectscripts.js
+++ resources/js/projectscripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     $('.js--prog').waypoint(function(direction) {
         $('.js--prog').addClass('animated slideInRight');
     }, {
-        offset: '70%'
+        offset: '80%'
 });
     $('.js--myweb').waypoint(function(direction) {
         $('.js--myweb').addClass('animated slideInLeft');
     }, {
-        offset: '70%'
+        offset: '80%'
 });
 });
